d4fae0749882994cd1b16ea436cc810b44d949c8
Lauris Bukšis-Haberkorns <lauris@nix.lv>
2007-01-31 22:01:44 +0000

    Test application is now console application so it could be posible to test it in Linux

--- Test/Form1.cs
+++ Test/Form1.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Drawing;
 using System.Collections;
-using System.ComponentModel;
-using System.Windows.Forms;
 using System.Data;
 using System.Text;
 
@@ -11,93 +8,17 @@ using Nix.SpreadSheet;
 namespace Test
 {
 	/// <summary>
-	/// Summary description for Form1.
+	/// Summary description for Test.
 	/// </summary>
-	public class Form1 : System.Windows.Forms.Form
+	public class Test
 	{
-        private System.Windows.Forms.Button button1;
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
-
-		public Form1()
-		{
-			//
-			// Required for Windows Form Designer support
-			//
-			InitializeComponent();
-
-			//
-			// TODO: Add any constructor code after InitializeComponent call
-			//
-		}
-
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if (components != null) 
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
-
-		#region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 8);
-            this.button1.Name = "button1";
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
-        }
-		#endregion
-
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
-		[STAThread]
-		static void Main() 
-		{
-			Application.Run(new Form1());
-		}
-
-        private void button1_Click(object sender, System.EventArgs e)
+        [STAThread]
+        public static void Main(string[] args)
         {
-            //try
-            //{
-                SpreadSheet ex = new SpreadSheet();
-                ex.Save("c:\\test.xls");
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}    
+        	SpreadSheet sp = new SpreadSheet();
+        	sp.Save("test.xls");
         }
-	}
 }
