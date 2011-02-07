e011d88ca4df54617be1bc670e5b36bb5e0d6ab6
Lauris Bukšis-Haberkorns <lauris@nix.lv>
2011-02-07 14:15:33 +0000

    Added CSV file format

---
+++ SpreadSheet/Provider/CsvFileFormatProvicer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Nix.SpreadSheet.Provider
+{
+    /// <summary>
+    /// CSV file format provider 
+    /// </summary>
+    public class CsvFileFormatProvicer : IFileFormatProvider
+    {
+        public void Save(SpreadSheetDocument document, System.IO.Stream stream)
+        {
+            foreach (Sheet s in document)
+            {
+                TextWriter t = new StreamWriter(stream, Encoding.UTF8);
+                for (int r = 0; r <= s.LastRow; r++ )
+                {
+                    for (int c = 0; c <= s.LastColumn; c++)
+                    {
+                        if (c != 0)
+                            t.Write(",");
+                        object val = s[r][c].Value;
+                        if (val == null || (val is string && (string)val == string.Empty))
+                            t.Write("\"\"");
+			            else if (val is int || val is float || val is double ||
+                	            val is decimal || val is long || val is short)
+            	            t.Write(val);
+			            else if (val is ErrorCode)
+                            t.Write("\"" + ((ErrorCode)val).ToString("g") + "\"");
+                        else
+                            t.Write("\"" + val.ToString().Replace("\"", "\"\"") + "\"");
+                    }
+                    t.WriteLine();
+                }
+                break;
+            }
+        }
+    }
+}
